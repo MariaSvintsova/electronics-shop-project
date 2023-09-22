8b15487369906f97ae3c2eaed823793f54ea86be
MariaSvintsova <m.svintsova@it-park.tech>
2023-09-22 13:04:49 +0300

    first changes

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item(name="Смартфон", price=10000, quantity=20)
+
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount():
+    item = Item(name="Смартфон", price=10000, quantity=20)
+    item.apply_discount()
+
+    assert item.price == 8000.0
+
+def test_all_items():
+    item1 = Item(name="Смартфон", price=10000, quantity=20)
+    item2 = Item(name="Ноутбук", price=20000, quantity=5)
+
+    assert Item.all[2:] == [item1, item2]
+
+
+
+
+
