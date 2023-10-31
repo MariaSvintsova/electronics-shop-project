aac0a4183f1680963e21c4027020bbe37aaf9edc
MariaSvintsova <m.svintsova@it-park.tech>
2023-10-31 23:50:51 +0300

    5 HW

--- src/phone.py
+++ src/phone.py
@@ -3,6 +3,7 @@ from src.item import Item
 
 class Phone(Item):
 
+
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
         self._number_of_sim = number_of_sim
